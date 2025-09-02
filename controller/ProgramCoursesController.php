8082f676fbc114ebfcdac23c1a062ec9786ba017
jil4school <jiliris.delacruz@my.nst.edu.ph>
2025-09-02 16:48:00 +0800

    header-origin

--- controller/ProgramCoursesController.php
+++ controller/ProgramCoursesController.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../ProgramCourses.php';
 
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
